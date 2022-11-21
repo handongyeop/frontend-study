085b4a6a05a8b3f743b8609f70f9118939bdf25e
dongyeop han <pig8776@naver.com>
2022-11-21 18:14:45 +0900

    피규어 섹션 추가 및 데이터 배열 표시

--- apple-ipad/js/main.js
+++ apple-ipad/js/main.js
@@ -1,3 +1,5 @@
+import ipads from "../data/ipads.js";
+
 // 장바구니 기능
 const basketStarterEl = document.querySelector("header .basket-starter");
 const basketEl = basketStarterEl.querySelector(".basket");
@@ -70,3 +72,63 @@ function hideSearch() {
   searchDelayEls.reverse();
   searchInputEl.value = "";
 }
+
+// 요소의 가시성 관찰
+const io = new IntersectionObserver(function (entries) {
+  entries.forEach(function (entry) {
+    if (!entry.isIntersecting) {
+      return;
+    }
+    entry.target.classList.add("show");
+  });
+});
+const infoEls = document.querySelectorAll(".info");
+infoEls.forEach(function (el) {
+  io.observe(el);
+});
+
+// 비디오 재생!
+const video = document.querySelector(".stage video");
+const playBtn = document.querySelector(".stage .controller--play");
+const pauseBtn = document.querySelector(".stage .controller--pause");
+
+playBtn.addEventListener("click", function () {
+  video.play();
+  playBtn.classList.add("hide");
+  pauseBtn.classList.remove("hide");
+});
+pauseBtn.addEventListener("click", function () {
+  video.pause();
+  pauseBtn.classList.add("hide");
+  playBtn.classList.remove("hide");
+});
+
+// '당신에게 맞는 iPad는?' 랜더링!
+const itemsEl = document.querySelector("section.compare .items");
+ipads.forEach(function (ipad) {
+  const itemEl = document.createElement("div");
+  itemEl.classList.add("item");
+
+  let colorList = "";
+  ipad.colors.forEach(function (color) {
+    colorList += /*html*/ `
+      <li style="background-color: ${color}"></li>
+    `;
+  });
+
+  itemEl.innerHTML = /* html*/ `
+    <div class="thumbnail">
+      <img src="${ipad.thumbnail}" alt="${ipad.name}" />
+    </div>
+    <ul class="colors">
+      ${colorList}
+    </ul>
+    <h3 class="name">${ipad.name}</h3>
+    <p class="tagline">${ipad.tagline}</p>
+    <p class="price">₩${ipad.price.toLocaleString("en-US")}부터</p>
+    <button class="btn">구입하기</button>
+    <a href="${ipad.url}" class="link">더 알아보기</a>
+  `;
+
+  itemsEl.append(itemEl);
+});
